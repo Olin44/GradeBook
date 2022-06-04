ddd7af627d4ab310bc465793ce39db6b2f630dd0
Shonsu <shonsu82@gmail.com>
2022-06-04 21:55:29 +0200

    added createExceptionString method, and fixed test

--- src/main/java/gradebook/Rate.java
+++ src/main/java/gradebook/Rate.java
@@ -3,13 +3,19 @@ package gradebook;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record Rate(double value) {
-    static List<Double> validRates = Arrays.asList(1.0, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0);
+    private static final List<Double> VALID_RATES = List.of(1.0, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0);
 
     public Rate {
-        if (!validRates.contains(value)) {
-            throw new InvalidRateValueException("Rate must be one of 1, 2, 2.5, 3, 3.5, 4, 4.5, 5.5, 6");
+        if (!VALID_RATES.contains(value)) {
+            throw new InvalidRateValueException(createExceptionString());
         }
+    }
+    private String createExceptionString() {
+        return VALID_RATES.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", ", "Rate must be one of ", ""));
     }
 }
--- src/test/java/gradebook/RateTest.java
+++ src/test/java/gradebook/RateTest.java
@@ -20,6 +20,6 @@ class RateTest {
     public void invalidRates(double rateValue) {
         var exception = assertThrows(InvalidRateValueException.class,
                 () -> new Rate(rateValue));
-        assertEquals(exception.getMessage(), "Rate must be one of 1, 2, 2.5, 3, 3.5, 4, 4.5, 5.5, 6");
+        assertEquals(exception.getMessage(), "Rate must be one of 1.0, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0");
     }
 }
