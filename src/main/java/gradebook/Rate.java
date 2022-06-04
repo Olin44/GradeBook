a075e156c327d01d9878b262ddf7a8c38235095f
Shonsu <shonsu82@gmail.com>
2022-06-04 21:27:21 +0200

    changed Rates validation in constructor

--- src/main/java/gradebook/Rate.java
+++ src/main/java/gradebook/Rate.java
@@ -1,10 +1,15 @@
 package gradebook;
 
-public record Rate(double value) {
-    public Rate {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-        if(value>6 || value<1 || (value % 0.5!=0) || value == 1.5){
-             throw new InvalidRateValueException("Rate must be one of 1, 2, 2.5, 3, 3.5, 4, 4.5, 5.5, 6");
+public record Rate(double value) {
+    static List<Double> validRates = Arrays.asList(1.0, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0);
+
+    public Rate {
+        if (!validRates.contains(value)) {
+            throw new InvalidRateValueException("Rate must be one of 1, 2, 2.5, 3, 3.5, 4, 4.5, 5.5, 6");
         }
     }
 }
