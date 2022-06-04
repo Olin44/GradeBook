e0d79436facec87453353b2f103ba6f32c803657
Shonsu <shonsu82@gmail.com>
2022-06-04 18:42:44 +0200

    ennum Rate created

--- src/main/java/gradebook/Rate.java
+++ src/main/java/gradebook/Rate.java
@@ -1,3 +1,18 @@
 package gradebook;
 
-public record Rate(double value) { }
+public enum Rate{
+    ONE (1),
+    TWO (2),
+    THREE (3),
+    FOUR (4),
+    FIVE (5),
+    SIX (6);
+
+    private final int value;
+    Rate(int i) {
+        this.value = i;
+    }
+    public int Value(){
+        return this.value;
+    }
+}
